afa697bcb10883eeb87fd1b5a531719788d320cf
lenechk <mikegreenbaum1@gmail.com>
2021-12-19 10:15:01 +0200

    updated traces script

--- helper_scripts/create_trace.sh
+++ helper_scripts/create_trace.sh
@@ -1,9 +1,13 @@
 cd ..
 # wget http://data.fcc.gov/download/measuring-broadband-america/2017/data-raw-2016-jan.tar.gz --no-check-certificate
-# tar -xvf data-raw-2016-jan.tar.gz
+# tar -zxvf data-raw-2016-jan.tar.gz 201601/curr_webget_2016_01.csv
+rm data-raw-2016-jan.tar.gz
 cd traces
 python3 load_webget_data.py --file ../201601/curr_webget_2016_01.csv
-python3 convert_mahimahi_format.py 
-python3 generate_train_test_traces.py 
-python3 split_train_test.py --traces ./final_traces/
+cd ..
+rm -rf 201601/
+cd traces
+python3 convert_mahimahi_format.py -c 700
+# python3 generate_train_test_traces.py 
+python3 split_train_test.py
 
